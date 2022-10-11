c419a5fdbf00ae4b86504158cc57070701db9ffb
Jandilson <jandilson.maia.jm@gmail.com>
2022-10-11 19:39:11 -0300

    ADD

--- Tarefa-LP/AT9.js
+++ Tarefa-LP/AT9.js
@@ -1,8 +1,14 @@
-let nome=["Q","U","E","C","A","S",'A','B','O','N','I','T','A']
-let e = nome.reduce((e,sum)=>{
-    return e+=sum;
+let frase="Olhei para frent"
+let e = list(frase)
+let lim = e.length
+let i = 0
 
-},)
-
+while (i < lim) {
+    if (e[i] === " ") {
+        delete f[i]
+        lim -=1
+    }
+    i+=1
+}
 console.log(e)
 
