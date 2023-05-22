baa5e5bae2ec0a6fdae26c55277c685b334aab8b
Troiando <jandilson.maia.jm@gmail.com>
2023-05-22 15:13:47 -0300

    ADD

---
+++ Aprendizado-HTML/TestandoHabilidade/script.js
@@ -0,0 +1,8 @@
+function calcularIdade() {
+    var dataNascimento = document.getElementById("dataNascimento").value;
+    var dataAtual = new Date();
+    var dataNascimentoFormatada = new Date(dataNascimento);
+    var diff = Math.abs(dataAtual - dataNascimentoFormatada);
+    var idade = Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25));
+    document.getElementById("idade").textContent = idade + " anos";
+}
